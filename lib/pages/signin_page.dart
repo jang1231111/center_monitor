c631f3b96e09bcf29c89a6ca1e1cf6445b3dde5d
jang1231111 <uiopzx9782@naver.com>
2024-09-05 15:52:23 +0900

    배포버전 (1.0.1) Add : SignIn Page 텍스트필드 scrollPadding 추가

--- lib/pages/signin_page.dart
+++ lib/pages/signin_page.dart
@@ -57,136 +57,140 @@ class _SigninPageState extends State<SigninPage> {
           child: GestureDetector(
             onTap: () => FocusScope.of(context).unfocus(),
             child: Scaffold(
-              body: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 30.0),
-                child: Form(
-                  key: _formKey,
-                  autovalidateMode: _autovalidateMode,
-                  child: ListView(
-                    shrinkWrap: true,
-                    children: [
-                      SizedBox(
-                        height: 130.0,
-                      ),
-                      Image.asset(
-                        'assets/images/background.jpeg',
-                        width: 100,
-                        height: 100,
-                      ),
-                      SizedBox(
-                        height: 80.0,
-                      ),
-                      Text(
-                        'Login',
-                        textAlign: TextAlign.center,
-                        style: loginTitle(context),
-                      ),
-                      SizedBox(
-                        height: 30.0,
-                      ),
-                      TextFormField(
-                        keyboardType: TextInputType.number,
-                        autocorrect: false,
-                        maxLength: 11,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          filled: true,
-                          labelText: 'Login Number',
-                          prefixIcon: Icon(Icons.login),
+              body: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                  child: Form(
+                    key: _formKey,
+                    autovalidateMode: _autovalidateMode,
+                    child: ListView(
+                      shrinkWrap: true,
+                      children: [
+                        SizedBox(
+                          height: 130.0,
                         ),
-                        validator: (String? value) {
-                          if (value == null || value.trim().isEmpty) {
-                            return '로그인 번호를 입려하세요';
-                          }
-                          if (value.trim().length < 11)
-                            return '로그인 번호는 11자리 전체를 입력해야 합니다.';
-                          return null;
-                        },
-                        onSaved: (String? inputPhoneNumber) {
-                          String phoneNumber = '';
-                          phoneNumber += inputPhoneNumber!.substring(0, 3);
-                          phoneNumber += '-';
-                          phoneNumber += inputPhoneNumber.substring(3, 7);
-                          phoneNumber += '-';
-                          phoneNumber += inputPhoneNumber.substring(7, 11);
-
-                          _phoneNumber = phoneNumber;
-                        },
-                      ),
-                      // TextButton.icon(
-                      //   onPressed: null,
-                      //   // centerListState.signinStatus == CenterListStatus.submitting
-                      //   //     ? null
-                      //   //     : () {
-                      //   //         Navigator.pushNamed(
-                      //   //             context, SignupPage.routeName);
-                      //   //       },
-                      //   icon: Icon(Icons.question_answer),
-                      //   label: Text('Forgot Login Number?'),
-                      //   style: TextButton.styleFrom(
-                      //       textStyle: TextStyle(
-                      //     fontSize: 10.0,
-                      //     decoration: TextDecoration.underline,
-                      //   )),
-                      // ),
-                      SizedBox(
-                        height: 20.0,
-                      ),
-                      SizedBox(height: 20.0),
-                      ElevatedButton(
-                        onPressed: centerListState.centerListStatus ==
-                                CenterListStatus.submitting
-                            ? null
-                            : _submit,
-                        child: Text(centerListState.centerListStatus ==
-                                CenterListStatus.submitting
-                            ? 'Loading...'
-                            : 'Sign in'),
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Color.fromARGB(255, 38, 94, 176),
-                          foregroundColor: Colors.white,
-                          textStyle: TextStyle(
-                            fontSize: 20.0,
-                            fontWeight: FontWeight.w600,
+                        Image.asset(
+                          'assets/images/background.jpeg',
+                          width: 100,
+                          height: 100,
+                        ),
+                        SizedBox(
+                          height: 80.0,
+                        ),
+                        Text(
+                          'Login',
+                          textAlign: TextAlign.center,
+                          style: loginTitle(context),
+                        ),
+                        SizedBox(
+                          height: 30.0,
+                        ),
+                        TextFormField(
+                          scrollPadding: EdgeInsets.only(
+                              bottom: MediaQuery.of(context).viewInsets.bottom),
+                          keyboardType: TextInputType.number,
+                          autocorrect: false,
+                          maxLength: 11,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            filled: true,
+                            labelText: 'Login Number',
+                            prefixIcon: Icon(Icons.login),
                           ),
-                          padding: const EdgeInsets.symmetric(vertical: 10.0),
+                          validator: (String? value) {
+                            if (value == null || value.trim().isEmpty) {
+                              return '로그인 번호를 입려하세요';
+                            }
+                            if (value.trim().length < 11)
+                              return '로그인 번호는 11자리 전체를 입력해야 합니다.';
+                            return null;
+                          },
+                          onSaved: (String? inputPhoneNumber) {
+                            String phoneNumber = '';
+                            phoneNumber += inputPhoneNumber!.substring(0, 3);
+                            phoneNumber += '-';
+                            phoneNumber += inputPhoneNumber.substring(3, 7);
+                            phoneNumber += '-';
+                            phoneNumber += inputPhoneNumber.substring(7, 11);
+
+                            _phoneNumber = phoneNumber;
+                          },
                         ),
-                      ),
-                      SizedBox(
-                        height: 30.0,
-                        child: Divider(
-                          height: 5,
+                        // TextButton.icon(
+                        //   onPressed: null,
+                        //   // centerListState.signinStatus == CenterListStatus.submitting
+                        //   //     ? null
+                        //   //     : () {
+                        //   //         Navigator.pushNamed(
+                        //   //             context, SignupPage.routeName);
+                        //   //       },
+                        //   icon: Icon(Icons.question_answer),
+                        //   label: Text('Forgot Login Number?'),
+                        //   style: TextButton.styleFrom(
+                        //       textStyle: TextStyle(
+                        //     fontSize: 10.0,
+                        //     decoration: TextDecoration.underline,
+                        //   )),
+                        // ),
+                        SizedBox(
+                          height: 20.0,
                         ),
-                      ),
-                      SizedBox(
-                        height: 50.0,
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                          color: Color.fromARGB(255, 244, 242, 242),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        width: double.infinity,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              '(주)옵티로',
-                              style: optilo_name(context),
+                        SizedBox(height: 20.0),
+                        ElevatedButton(
+                          onPressed: centerListState.centerListStatus ==
+                                  CenterListStatus.submitting
+                              ? null
+                              : _submit,
+                          child: Text(centerListState.centerListStatus ==
+                                  CenterListStatus.submitting
+                              ? 'Loading...'
+                              : 'Sign in'),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Color.fromARGB(255, 38, 94, 176),
+                            foregroundColor: Colors.white,
+                            textStyle: TextStyle(
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.w600,
                             ),
-                            Text(
-                              '인천광역시 연수구 송도미래로 30 스마트밸리 D동',
-                              style: optilo_info(context),
-                            ),
-                            Text(
-                              'H : www.optilo.net  T : 070-5143-8585',
-                              style: optilo_info(context),
-                            ),
-                          ],
+                            padding: const EdgeInsets.symmetric(vertical: 10.0),
+                          ),
                         ),
-                      ),
-                    ],
+                        SizedBox(
+                          height: 30.0,
+                          child: Divider(
+                            height: 5,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 50.0,
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 244, 242, 242),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          width: double.infinity,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                '(주)옵티로',
+                                style: optilo_name(context),
+                              ),
+                              Text(
+                                '인천광역시 연수구 송도미래로 30 스마트밸리 D동',
+                                style: optilo_info(context),
+                              ),
+                              Text(
+                                'H : www.optilo.net  T : 070-5143-8585',
+                                style: optilo_info(context),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
