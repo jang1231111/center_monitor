c57ad6aff2b4d3736c9b1ccb629f4487f998e84f
jang1231111 <uiopzx9782@naver.com>
2024-09-20 09:09:18 +0900

    fix : 로그인 Null 쿼리 수정

--- lib/serivices/api_services.dart
+++ lib/serivices/api_services.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
+import 'package:center_monitor/constants/constants.dart';
 import 'package:center_monitor/models/a10_model.dart';
 import 'package:center_monitor/models/log_data_model.dart';
+import 'package:center_monitor/models/login_request.dart';
+import 'package:center_monitor/serivices/http_error_handler.dart';
 import 'package:http/http.dart' as http;
 import 'package:mysql_client/mysql_client.dart';
 
@@ -54,7 +59,7 @@ class ApiServices {
     await conn.connect();
 
     var result = await conn.execute(
-        "SELECT * FROM (SELECT DISTINCT a.de_number, a.center_sn, a.de_name, TRUNCATE(b.temp, 1) AS temp, TRUNCATE(b.hum, 1) AS hum, b.battery, a.de_location, c.temp_low, c.temp_high, c.hum_low, c.hum_high, DATE_FORMAT(b.datetime, '%Y-%m-%d %H:%i') AS `datetime`, DATE_FORMAT(a.start_time, '%Y-%m-%d %H:%i') AS start_time, DATE_FORMAT(a.end_time, '%Y-%m-%d %H:%i') AS end_time, b.position_x, b.position_y FROM CENTER_HISTORY a LEFT JOIN CENTER_DEVICE b ON a.de_number = b.de_number AND a.record_date = DATE(b.datetime) AND b.`status` = 1 LEFT JOIN LIMIT_INFO c ON b.de_location = c.de_location WHERE a.center_sn = 1101 AND a.status = 1 AND a.record_date > DATE_SUB(CURDATE(), INTERVAL 2 MONTH) ORDER BY b.datetime DESC , a.record_date DESC) A GROUP BY A.de_number , A.center_sn , A.de_name ORDER BY A.de_name");
+        "SELECT * FROM (SELECT DISTINCT a.de_number, a.center_sn, a.de_name, TRUNCATE(b.temp, 1) AS temp, TRUNCATE(b.hum, 1) AS hum, b.battery, a.de_location, c.temp_low, c.temp_high, c.hum_low, c.hum_high, DATE_FORMAT(b.datetime, '%Y-%m-%d %H:%i') AS `datetime`, DATE_FORMAT(a.start_time, '%Y-%m-%d %H:%i') AS start_time, DATE_FORMAT(a.end_time, '%Y-%m-%d %H:%i') AS end_time, b.position_x, b.position_y FROM CENTER_HISTORY a LEFT JOIN CENTER_DEVICE b ON a.de_number = b.de_number AND a.record_date = DATE(b.datetime) AND b.`status` = 1 LEFT JOIN LIMIT_INFO c ON b.de_location = c.de_location WHERE a.center_sn = 1101 AND a.status = 1 AND a.record_date = CURDATE()ORDER BY b.datetime DESC , a.record_date DESC) A GROUP BY A.de_number , A.center_sn , A.de_name ORDER BY A.de_name");
 
     final List<A10> deviceList = [];
     A10 a10;
@@ -143,42 +148,45 @@ class ApiServices {
     return logDatas;
   }
 
-  // Future<Centerinfo> getCenterInfo(String phoneNumber) async {
-  //   final Map<String, dynamic> data = new Map<String, dynamic>();
-  //   final Map<String, dynamic> response = new Map<String, dynamic>();
-  //   final Map<String, dynamic> common = new Map<String, dynamic>();
+  Future<void> getCenterInfo(String phoneNumber) async {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    // final Map<String, dynamic> response = new Map<String, dynamic>();
+    // final Map<String, dynamic> common = new Map<String, dynamic>();
 
-  //   common['managerPn'] = phoneNumber;
-  //   response["common"] = common;
-  //   data["response"] = response;
+    data['userId'] = 'admin';
+    data["password"] = 'admin123';
 
-  //   var client = http.Client();
-  //   var uri = Uri.parse(kLoginUri);
+    var client = http.Client();
+    var uri = Uri.parse(kLoginUri);
 
-  //   try {
-  //     final http.Response response = await client.post(uri,
-  //         headers: {"Content-Type": "application/json"},
-  //         body: jsonEncode(data));
+    try {
+      final http.Response response = await client.post(uri,
+          headers: {"Content-Type": "application/json"},
+          body: jsonEncode(data));
 
-  //     if (response.statusCode != 200) {
-  //       throw Exception(httpErrorHandler(response));
-  //     }
+      /// 인터셉터 참고하면 더 편할 듯
+      ///  원래 기본 헤더 authenti 머시기
+      ///  baer ? + 토큰 넣기
 
-  //     final responseBody = json.decode(response.body);
+      if (response.statusCode != 200) {
+        throw Exception(httpErrorHandler(response));
+      }
 
-  //     if (responseBody.toString() ==
-  //         '{msg: No Data In MANAGER_INFO, notice: []}') {
-  //       throw Exception('전화번호를 확인해 주세요');
-  //     }
+      final responseBody = json.decode(response.body);
 
-  //     // print(responseBody.toString());
-  //     final centerInfo = Centerinfo.fromJson(responseBody);
+      if (responseBody.toString() ==
+          '{msg: No Data In MANAGER_INFO, notice: []}') {
+        throw Exception('전화번호를 확인해 주세요');
+      }
 
-  //     return centerInfo;
-  //   } catch (e) {
-  //     rethrow;
-  //   }
-  // }
+      print(responseBody.toString());
+      // final centerInfo = Centerinfo.fromJson(responseBody);
+
+      // return centerInfo;
+    } catch (e) {
+      rethrow;
+    }
+  }
 
   // Future<List<A10>> getDeviceList(Centerinfo centerInfo) async {
   //   final Map<String, dynamic> data = new Map<String, dynamic>();
