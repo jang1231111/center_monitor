566db5acf8f1f5d292a0e7bd160144c1b6af6eea
jang1231111 <uiopzx9782@naver.com>
2025-02-10 14:19:58 +0900

    chore : wake_lock 패키지 수정

--- lib/main.dart
+++ lib/main.dart
@@ -18,13 +18,13 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:http/http.dart' as http;
-import 'package:wakelock/wakelock.dart';
+import 'package:wakelock_plus/wakelock_plus.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await EasyLocalization.ensureInitialized();
-  Wakelock.enable();
+  WakelockPlus.enable();
 
   runApp(
     EasyLocalization(
